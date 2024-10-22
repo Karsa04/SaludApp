5554669a1d90e1d0b9f5a8e785ec00f4d191cf0a
Karsa04 <carlessaezna04@gmai.com>
2024-10-22 12:57:19 +0200

    Version 1 pantalla toast(comentado) y snackbar

--- app/src/main/java/com/example/saludapp/MainActivity.kt
+++ app/src/main/java/com/example/saludapp/MainActivity.kt
@@ -1,20 +1,44 @@
 package com.example.saludapp
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.snackbar.Snackbar
+import kotlin.math.log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        val btnHello:androidx.appcompat.widget.AppCompatButton= findViewById(R.id.btnHello)
+        val etName:EditText =findViewById(R.id.etName)
+
+
+           btnHello.setOnClickListener {
+               //Log.i("test", "Botón pulsado ")
+               val name =etName.text.toString()
+              // Toast.makeText(this,"Hola $name!", Toast.LENGTH_LONG).show()
+               if (name.isNotEmpty()){
+                   Snackbar.make(btnHello, "Hola $name!", Snackbar.LENGTH_SHORT)
+                       .setAnchorView(btnHello)
+                       .setAction("Reset") {etName.setText(null) }
+                       .show()
+               }
+
+           }
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+
     }
 }
